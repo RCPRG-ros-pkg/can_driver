e7dacee189b5675c52e1951a3c401a2a1e3d9247
Tomasz Winiarski <tomrobotics@gmail.com>
2016-08-18 16:11:09 +0200

    code formating

--- include/can_driver/CANDev.h
+++ include/can_driver/CANDev.h
@@ -4,14 +4,14 @@
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of the Warsaw University of Technology nor the
-       names of its contributors may be used to endorse or promote products
-       derived from this software without specific prior written permission.
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the Warsaw University of Technology nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
  
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -23,56 +23,60 @@
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
-#ifndef _CAN_DEV_H_
-#define _CAN_DEV_H_
-
-#include <string>
-#include <vector>
-#include <inttypes.h>
+#ifndef CANDEV_H_
+#define CANDEV_H_
 
 #if !defined(HAVE_RTNET)
 #include <linux/can.h>
 #include <linux/can/raw.h>
 #else
-#include <rtdm/rtcan.h> // Defines for the RT CAN socket
+#include <rtdm/rtcan.h>  // Defines for the RT CAN socket
 #endif
+
+#include <inttypes.h>
+
+#include <string>
+#include <vector>
+
+
 
 class CANDev {
-public:
+ public:
+  typedef uint8_t Header[4];
 
-    typedef uint8_t Header[4];
+  class FilterElement {
+   public:
+    FilterElement(uint32_t can_id, uint32_t can_mask);
+    uint32_t can_id_;
+    uint32_t can_mask_;
+  };
 
-    class FilterElement {
-    public:
-        FilterElement(uint32_t can_id, uint32_t can_mask);
-        uint32_t can_id_;
-        uint32_t can_mask_;
-    };
+  CANDev(const std::string &dev_name, const std::string &name,
+         const std::vector<FilterElement> &filter_vec);
+  ~CANDev();
 
-    CANDev(const std::string &dev_name, const std::string &name, const std::vector<FilterElement > &filter_vec);
-    ~CANDev();
+  void setMultiFrameReceiver(uint32_t nbytes);
+  uint32_t readMultiFrameData(uint32_t can_id, uint8_t *data,
+                              const CANDev::Header &header);
 
-    void setMultiFrameReceiver(uint32_t nbytes);
-    uint32_t readMultiFrameData(uint32_t can_id, uint8_t *data, CANDev::Header &header);
+  bool send(uint32_t can_id, uint8_t len, const uint8_t *data);
+  uint32_t waitForReply(uint32_t can_id, uint8_t *data);
+  bool isOpened();
 
-    bool send(uint32_t can_id, uint8_t len, const uint8_t *data);
-    uint32_t waitForReply(uint32_t can_id, uint8_t *data);
-    bool isOpened();
-protected:
+ protected:
+  int dev;
 
-    int dev;
+  std::vector<can_frame> frame_buf;
+  int buf_size;
 
-    std::vector<can_frame> frame_buf;
-    int buf_size;
-
-    std::vector<can_frame> multi_frame_buf;
-    int multi_buf_size;
-    uint32_t multi_nbytes;
-    std::string dev_name_;
-    std::string name_;
+  std::vector<can_frame> multi_frame_buf;
+  int multi_buf_size;
+  uint32_t multi_nbytes;
+  std::string dev_name_;
+  std::string name_;
 };
 
-#endif
+#endif  // CANDEV_H_
 
--- src/CANDev.cpp
+++ src/CANDev.cpp
@@ -4,14 +4,14 @@
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of the Warsaw University of Technology nor the
-       names of its contributors may be used to endorse or promote products
-       derived from this software without specific prior written permission.
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the Warsaw University of Technology nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
  
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -23,7 +23,7 @@
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #include <net/if.h>
 #include <sys/types.h>
@@ -32,9 +32,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#include <iostream>
+#include <iostream> // NOLINT
 #include <cstring>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 #include "can_driver/CANDev.h"
 
@@ -49,98 +51,104 @@
 #endif
 
 CANDev::FilterElement::FilterElement(uint32_t can_id, uint32_t can_mask) {
-    can_id_ = can_id;
-    can_mask_ = can_mask;
+  can_id_ = can_id;
+  can_mask_ = can_mask;
 }
 
-CANDev::CANDev(const std::string &dev_name, const std::string &name, const std::vector<CANDev::FilterElement > &filter_vec) :
-    frame_buf(1000),
-    buf_size(0),
-    dev_name_(dev_name),
-    name_(name)
-{
-    struct sockaddr_can addr;
-    struct ifreq ifr;
-  
-    if((dev = rt_dev_socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-        std::cout<< "ERROR: CANDev::CANDev(" << dev_name_ << ", " << name_ << "): error in opening socket" << std::endl;
-        dev = -1;
-        return;
+CANDev::CANDev(const std::string &dev_name, const std::string &name,
+               const std::vector<CANDev::FilterElement> &filter_vec)
+    : frame_buf(1000),
+      buf_size(0),
+      dev_name_(dev_name),
+      name_(name) {
+  struct sockaddr_can addr;
+  struct ifreq ifr;
+
+  if ((dev = rt_dev_socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+    std::cout << "ERROR: CANDev::CANDev(" << dev_name_ << ", " << name_
+              << "): error in opening socket" << std::endl;
+    dev = -1;
+    return;
+  }
+
+  strcpy(ifr.ifr_name, dev_name.c_str());  // NOLINT
+  rt_dev_ioctl(dev, SIOCGIFINDEX, &ifr);
+
+  addr.can_family = AF_CAN;
+  addr.can_ifindex = ifr.ifr_ifindex;
+
+  int nfilters = filter_vec.size();
+  if (nfilters > 0) {
+    struct can_filter *rfilter = new can_filter[nfilters];
+    for (int i = 0; i < nfilters; i++) {
+      rfilter[i].can_id = filter_vec[i].can_id_;
+      rfilter[i].can_mask = filter_vec[i].can_mask_;
     }
 
-    strcpy(ifr.ifr_name, dev_name.c_str());
-    rt_dev_ioctl(dev, SIOCGIFINDEX, &ifr);
-
-    addr.can_family  = AF_CAN;
-    addr.can_ifindex = ifr.ifr_ifindex; 
-
-    int nfilters = filter_vec.size();
-    if (nfilters > 0) {
-        struct can_filter *rfilter = new can_filter[nfilters];
-        for (int i = 0; i < nfilters; i++) {
-            rfilter[i].can_id   = filter_vec[i].can_id_;
-            rfilter[i].can_mask = filter_vec[i].can_mask_;
-        }
-
-        if (rt_dev_setsockopt(dev, SOL_CAN_RAW, CAN_RAW_FILTER, rfilter,
-                                            sizeof(struct can_filter) * nfilters) < 0)
-        {
-            std::cout << "ERROR: CANDev::CANDev(" << dev_name_ << ", " << name_ << "): filter" << std::endl;
-            rt_dev_close(dev);
-            delete[] rfilter;
-            dev = -1;
-            return;
-        }
-
-        delete[] rfilter;
+    if (rt_dev_setsockopt(dev, SOL_CAN_RAW, CAN_RAW_FILTER, rfilter,
+                          sizeof(struct can_filter) * nfilters) < 0) {
+      std::cout << "ERROR: CANDev::CANDev(" << dev_name_ << ", " << name_
+                << "): filter" << std::endl;
+      rt_dev_close(dev);
+      delete[] rfilter;
+      dev = -1;
+      return;
     }
 
-//#if !defined(HAVE_RTNET)
-    // TODO: check if it is working in RT
-    {
-        struct timeval timeout;
-        timeout.tv_sec = 1;
-        timeout.tv_usec = 0;
-        if (setsockopt(dev, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(struct timeval)) < 0) {
-            std::cout << "ERROR: CANDev::CANDev: setsockopt SO_RCVTIMEO" << std::endl;
-            rt_dev_close(dev);
-            dev = -1;
-            return;
-        }
-        if (setsockopt(dev, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(struct timeval)) < 0) {
-            std::cout << "ERROR: CANDev::CANDev: setsockopt SO_SNDTIMEO" << std::endl;
-            rt_dev_close(dev);
-            dev = -1;
-            return;
-        }
+    delete[] rfilter;
+  }
+
+// #if !defined(HAVE_RTNET)
+  // TODO(my_username): check if it is working in RT
+  {
+    struct timeval timeout;
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+    if (setsockopt(dev, SOL_SOCKET, SO_RCVTIMEO, &timeout,
+                   sizeof(struct timeval)) < 0) {
+      std::cout << "ERROR: CANDev::CANDev: setsockopt SO_RCVTIMEO" << std::endl;
+      rt_dev_close(dev);
+      dev = -1;
+      return;
     }
-//#else
+    if (setsockopt(dev, SOL_SOCKET, SO_SNDTIMEO, &timeout,
+                   sizeof(struct timeval)) < 0) {
+      std::cout << "ERROR: CANDev::CANDev: setsockopt SO_SNDTIMEO" << std::endl;
+      rt_dev_close(dev);
+      dev = -1;
+      return;
+    }
+  }
+// #else
 //    // one second timeout for receive and send
 //    rt_dev_ioctl(dev, RTCAN_RTIOC_RCV_TIMEOUT, 1000000000UL);
 //    rt_dev_ioctl(dev, RTCAN_RTIOC_SND_TIMEOUT, 1000000000UL);
-//#endif
+// #endif
 
-    if(rt_dev_bind(dev, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        std::cout << "ERROR: CANDev::CANDev(" << dev_name_ << ", " << name_ << "): error in socket bind" << std::endl;
-        rt_dev_close(dev);
-        dev = -1;
-    }
+  if (rt_dev_bind(dev, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+    std::cout << "ERROR: CANDev::CANDev(" << dev_name_ << ", " << name_
+              << "): error in socket bind" << std::endl;
+    rt_dev_close(dev);
+    dev = -1;
+  }
 }
 
 CANDev::~CANDev() {
-  if(dev > -1) {
+  if (dev > -1) {
     rt_dev_close(dev);
   }
 }
 
-bool CANDev::send(const uint32_t can_id, const uint8_t len, const uint8_t *data) {
+bool CANDev::send(const uint32_t can_id, const uint8_t len,
+                  const uint8_t *data) {
   struct can_frame frame;
-  
+
   frame.can_id = can_id;
   frame.can_dlc = len;
-  
+
   memcpy(frame.data, data, len);
-  size_t ret = rt_dev_send(dev, reinterpret_cast<void*>(&frame), sizeof(frame), 0);
+  size_t ret = rt_dev_send(dev, reinterpret_cast<void*>(&frame), sizeof(frame),
+                           0);
 
   if (ret == sizeof(frame)) {
     return true;
@@ -150,88 +158,94 @@ bool CANDev::send(const uint32_t can_id, const uint8_t len, const uint8_t *data)
 }
 
 uint32_t CANDev::waitForReply(uint32_t can_id, uint8_t *data) {
-    struct can_frame frame;
+  struct can_frame frame;
 
-    // search frame buffer
-    for(size_t i = 0; i < buf_size; i++) {
-        if(frame_buf[i].can_id == can_id) {
-            memcpy(data, frame_buf[i].data, frame_buf[i].can_dlc);
-            uint8_t can_dlc = frame_buf[i].can_dlc;
+  // search frame buffer
+  for (size_t i = 0; i < buf_size; i++) {
+    if (frame_buf[i].can_id == can_id) {
+      memcpy(data, frame_buf[i].data, frame_buf[i].can_dlc);
+      uint8_t can_dlc = frame_buf[i].can_dlc;
 
-            // erase
-            for (int j = i+1; j < buf_size; j++) {
-                frame_buf[j-1] = frame_buf[j];
-            }
-            buf_size--;
-            return can_dlc;
-        }
+      // erase
+      for (int j = i + 1; j < buf_size; j++) {
+        frame_buf[j - 1] = frame_buf[j];
+      }
+      buf_size--;
+      return can_dlc;
+    }
+  }
+
+  // wait for new data
+  while (1) {
+    size_t ret = rt_dev_recv(dev, reinterpret_cast<void*>(&frame),
+                             sizeof(frame), 0);
+    if (ret != sizeof(frame)) {
+      return 0;
     }
 
-    // wait for new data
-    while(1) {
-        size_t ret = rt_dev_recv(dev, reinterpret_cast<void*>(&frame), sizeof(frame), 0);
-        if(ret != sizeof(frame)) {
-            //std::cout << "CANDev::waitForReply(" << dev_name_ << ", " << name_ << "): rt_dev_recv could not read" << std::endl;
-            return 0;
-        }
-
-        if(frame.can_id == can_id) {
-            memcpy(data, frame.data, frame.can_dlc);
-            return frame.can_dlc;
-        }
-        if (buf_size >= frame_buf.size()) {
-            //std::cout << "CANDev::waitForReply(" << dev_name_ << ", " << name_ << "): buffer is full" << std::endl;
-            std::cout << std::endl;
-            return 0;
-        }
-        frame_buf[buf_size] = frame;
-        buf_size++;
+    if (frame.can_id == can_id) {
+      memcpy(data, frame.data, frame.can_dlc);
+      return frame.can_dlc;
     }
+    if (buf_size >= frame_buf.size()) {
+      // std::cout << "CANDev::waitForReply(" << dev_name_ << ", " << name_ << "): buffer is full" << std::endl;
+      // std::cout << std::endl;
+      return 0;
+    }
+    frame_buf[buf_size] = frame;
+    buf_size++;
+  }
 }
 
 bool CANDev::isOpened() {
-	return dev != -1;
+  return dev != -1;
 }
 
 void CANDev::setMultiFrameReceiver(uint32_t nbytes) {
-    multi_buf_size = 0;
-    multi_frame_buf.resize( (nbytes+7)/8 );
-    multi_nbytes = nbytes;
+  multi_buf_size = 0;
+  multi_frame_buf.resize((nbytes + 7) / 8);
+  multi_nbytes = nbytes;
 }
 
-uint32_t CANDev::readMultiFrameData(uint32_t can_id, uint8_t *data, CANDev::Header &header) {
-    struct can_frame frame;
+uint32_t CANDev::readMultiFrameData(uint32_t can_id, uint8_t *data,
+                                    const CANDev::Header &header) {
+  struct can_frame frame;
 
-    if (multi_buf_size <= 0) {
-        while(1) {
-            size_t ret = rt_dev_recv(dev, reinterpret_cast<void*>(&frame), sizeof(frame), 0);
-            if(ret != sizeof(frame)) {
-                return 0;
-            }
-            if(frame.can_id == can_id && header[0] == frame.data[0] && header[1] == frame.data[1] && header[2] == frame.data[2] && header[3] == frame.data[3]) {
-                multi_frame_buf[0] = frame;
-                multi_buf_size = 1;
-                break;
-            }
-        }
+  if (multi_buf_size <= 0) {
+    while (1) {
+      size_t ret = rt_dev_recv(dev, reinterpret_cast<void*>(&frame),
+                               sizeof(frame), 0);
+      if (ret != sizeof(frame)) {
+        return 0;
+      }
+      if (frame.can_id == can_id && header[0] == frame.data[0]
+          && header[1] == frame.data[1] && header[2] == frame.data[2]
+          && header[3] == frame.data[3]) {
+        multi_frame_buf[0] = frame;
+        multi_buf_size = 1;
+        break;
+      }
     }
+  }
 
-    while(1) {
-        size_t ret = rt_dev_recv(dev, reinterpret_cast<void*>(&frame), sizeof(frame), 0);
-        if(ret != sizeof(frame)) {
-            return 0;
-        }
-        if(frame.can_id == can_id) {
-            multi_frame_buf[multi_buf_size] = frame;
-            multi_buf_size++;
-            if (multi_buf_size == multi_frame_buf.size()) {
-                for (int i = 0, frame_idx=0; i < multi_nbytes; i += 8, frame_idx++) {
-                    memcpy(&data[i], multi_frame_buf[frame_idx].data, std::min(8u, multi_nbytes - i));
-                }
-                multi_buf_size = 0;
-                return multi_nbytes;
-            }
-        }
+  while (1) {
+    size_t ret = rt_dev_recv(dev, reinterpret_cast<void*>(&frame),
+                             sizeof(frame), 0);
+    if (ret != sizeof(frame)) {
+      return 0;
     }
+    if (frame.can_id == can_id) {
+      multi_frame_buf[multi_buf_size] = frame;
+      multi_buf_size++;
+      if (multi_buf_size == multi_frame_buf.size()) {
+        for (int i = 0, frame_idx = 0; i < multi_nbytes; i += 8, frame_idx++) {
+          memcpy(&data[i], multi_frame_buf[frame_idx].data,
+                 std::min(8u, multi_nbytes - i));
+        }
+        multi_buf_size = 0;
+        return multi_nbytes;
+      }
+    }
+  }
 }
 
